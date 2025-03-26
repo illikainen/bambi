527c58f88d3fab4b385fd0d3940fcff928b73d2b
Hans Jerry Illikainen <hji@dyntopia.com>
2025-03-26 05:10:03 +0000

    cmd: seal: remove the per-subcommand sandbox

--- src/cmd/seal.go
+++ src/cmd/seal.go
@@ -9,8 +9,6 @@ import (
 	"github.com/illikainen/go-cryptor/src/blob"
 	"github.com/illikainen/go-utils/src/errorx"
 	"github.com/illikainen/go-utils/src/flag"
-	"github.com/illikainen/go-utils/src/process"
-	"github.com/illikainen/go-utils/src/sandbox"
 	"github.com/pkg/errors"
 	"github.com/samber/lo"
 	log "github.com/sirupsen/logrus"
@@ -18,6 +16,7 @@ import (
 )
 
 var sealOpts struct {
+	input  flag.PathSlice
 	output flag.Path
 }
 
@@ -31,57 +30,42 @@ var sealCmd = &cobra.Command{
 			log.Fatalf("%s", err)
 		}
 	},
-	Args: func(_ *cobra.Command, args []string) error {
-		if len(args) <= 0 {
-			return errors.Errorf("no files to seal")
-		}
-		return nil
-	},
+	Args: sealArgs,
 }
 
 func init() {
 	flags := sealCmd.Flags()
 
+	sealOpts.input.State = flag.MustExist
+	flags.VarP(&sealOpts.input, "input", "i", "Input file(s) to seal")
+	lo.Must0(flags.MarkHidden("input"))
+
 	sealOpts.output.State = flag.MustNotExist
+	sealOpts.output.Mode = flag.ReadWriteMode
 	flags.VarP(&sealOpts.output, "output", "o", "Output file for the sealed blob")
 	lo.Must0(sealCmd.MarkFlagRequired("output"))
 
 	rootCmd.AddCommand(sealCmd)
 }
 
-func sealRun(_ *cobra.Command, args []string) (err error) {
-	if sandbox.Compatible() && !sandbox.IsSandboxed() {
-		ro := []string{}
-		rw := []string{sealOpts.output.String()}
-
-		confRO, confRW, err := rootOpts.config.SandboxPaths()
-		if err != nil {
-			return err
-		}
-		ro = append(ro, confRO...)
-		rw = append(rw, confRW...)
-
-		// Required to mount the file in the sandbox.
-		f, err := os.Create(sealOpts.output.String())
-		if err != nil {
-			return err
-		}
-
-		err = f.Close()
-		if err != nil {
-			return err
-		}
-
-		_, err = sandbox.Exec(sandbox.Options{
-			Command: os.Args,
-			RO:      append(ro, args...),
-			RW:      rw,
-			Stdout:  process.LogrusOutput,
-			Stderr:  process.LogrusOutput,
-		})
-		return err
+func sealArgs(cmd *cobra.Command, args []string) error {
+	if len(args) <= 0 {
+		return errors.Errorf("no files to seal")
 	}
 
+	flags := cmd.Flags()
+	input := flags.Lookup("input")
+	for _, arg := range args {
+		err := input.Value.Set(arg)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func sealRun(_ *cobra.Command, args []string) (err error) {
 	keys, err := blob.ReadKeyring(rootOpts.privKey.String(), rootOpts.pubKeys.StringSlice())
 	if err != nil {
 		return err
