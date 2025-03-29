dce7c8838857e0571d7691341b05ce173fd6e02e
Hans Jerry Illikainen <hji@dyntopia.com>
2025-03-29 04:44:57 +0000

    cmd: add verify command

---
+++ src/cmd/verify.go
@@ -0,0 +1,71 @@
+package cmd
+
+import (
+	"io"
+	"os"
+
+	"github.com/illikainen/bambi/src/metadata"
+
+	"github.com/illikainen/go-cryptor/src/blob"
+	"github.com/illikainen/go-utils/src/cobrax"
+	"github.com/illikainen/go-utils/src/errorx"
+	"github.com/illikainen/go-utils/src/flag"
+	"github.com/samber/lo"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+var verifyOpts struct {
+	input      flag.Path
+	signedOnly bool
+}
+
+var verifyCmd = &cobra.Command{
+	Use:   "verify",
+	Short: "Verify a signed and encrypted archive",
+	Run:   cobrax.Run(verifyRun),
+}
+
+func init() {
+	flags := verifyCmd.Flags()
+
+	verifyOpts.input.State = flag.MustExist
+	flags.VarP(&verifyOpts.input, "input", "i", "File to verify")
+	lo.Must0(verifyCmd.MarkFlagRequired("input"))
+
+	flags.BoolVarP(&verifyOpts.signedOnly, "signed-only", "s", false,
+		"Required if the archive is signed but not encrypted")
+
+	rootCmd.AddCommand(verifyCmd)
+}
+
+func verifyRun(_ *cobra.Command, _ []string) (err error) {
+	keys, err := blob.ReadKeyring(rootOpts.privKey.String(), rootOpts.pubKeys.StringSlice())
+	if err != nil {
+		return err
+	}
+
+	inf, err := os.Open(verifyOpts.input.String())
+	if err != nil {
+		return err
+	}
+	defer errorx.Defer(inf.Close, &err)
+
+	blobber, err := blob.NewReader(inf, &blob.Options{
+		Type:      metadata.Name(),
+		Keyring:   keys,
+		Encrypted: !verifyOpts.signedOnly,
+	})
+	if err != nil {
+		return err
+	}
+
+	// Not strictly needed because the blob is verified in NewReader().
+	_, err = io.Copy(io.Discard, blobber)
+	if err != nil {
+		return nil
+	}
+
+	log.Infof("successfully verified %s", verifyOpts.input.String())
+	return nil
+}
